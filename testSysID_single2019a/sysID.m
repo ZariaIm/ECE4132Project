b1b189799823ad9dc39f45eee1b90163e9945469
ZariaIm <61674346+ZariaIm@users.noreply.github.com>
2020-08-31 21:00:58 +1000

    Added Files From Moodle

---
+++ testSysID_single2019a/sysID.m
@@ -0,0 +1,39 @@
+function [TF, IC] = sysID(patient)
+% Use this template to design an open-loop system identification routine given
+% the step time response of the patient. 
+
+%% input response
+% The input response is loaded here and used to simulate the patient to produce 
+% the step response. Feel free to alter this section as needed to try different
+% types of inputs that may help with the identification process
+[time_vec, Food, InsulinRate] = inputVector();
+
+% Simulate the open loop response of the generated patient
+Sugar = openLoopSim(patient,Food,InsulinRate);
+
+% Get Sugar values at time_vec time. This is basic linear interpolation and
+% is nessesary because Simulink does not guarantee Sugar.Time will equal time_vec
+sugar_vec = interp1(Sugar.Time,Sugar.Data,time_vec,'linear');
+
+%% system identification
+
+% Here are some potentially useful functions:
+% - findpeak
+% - min/max
+
+%Find the peaks in the data, this is when the slope changes sign
+[PKS,LOCS] = findpeaks(sugar_vec,time_vec);
+
+%Fine the min and max 
+min_val = min(sugar_vec);
+max_val = max(sugar_vec);
+
+%In the example we ignore the peaks, min and max and just output a first order 
+%response shifted by 160 
+%Produce first order transfer function as output
+s = tf('s');
+TF = -80*(4/(10*60))/(s+4/(10*60));
+%Produce initial condition (offset from zero)
+IC = 160;
+
+end
